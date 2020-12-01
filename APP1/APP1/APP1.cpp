e5f797cad762b0abc80e6d7ab3d235b16b62db08
koreait24 <59995434+koreait24@users.noreply.github.com>
2020-12-01 17:07:55 +0900

    git add APP1.cpp

--- APP1/APP1/APP1.cpp
+++ APP1/APP1/APP1.cpp
@@ -3,11 +3,17 @@
 
 #include "stdafx.h"
 #include <Windows.h>
-
+#include "header1.h"
 
 int main()
 {
+	double a, b;
+	a = 8.0f, b = 5.0f;
+
 	printf("Hello\n");
-    return 0;
+	printf("%.2f", mpow(a, b));
+
+	system("pause");
+	return 0;
 }
 
